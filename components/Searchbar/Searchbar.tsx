b1589b079d3d411c08ca9f7407abef2d14c868ba
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-09-07 19:32:41 -0500

    Add Searchbar component

---
+++ components/Searchbar/Searchbar.tsx
@@ -0,0 +1,50 @@
+import Input from 'components/Input';
+import React, { FC } from 'react';
+
+// https://randomuser.me/api
+interface Searchbar {
+	/** User record from an api */
+	user?: any;
+	loading: boolean;
+}
+
+const Searchbar: FC<Searchbar> = ({ user, loading }) => {
+	const name = getName(user);
+
+	return (
+		<div className="flex items-center justify-end bg-secondary w-full h-20 p-6">
+			<Input rightIcon="/assets/svgs/search.svg" />
+			<div className="w-px h-full bg-white mx-4 text-sm" />
+			{!loading && user && <UserInfo name={name} picture={user.picture.thumbnail} />}
+			<button>
+				<img src="/assets/svgs/log-out.svg" alt="Cerrar Sesión" />
+			</button>
+		</div>
+	);
+};
+
+interface UserInfoProps {
+	name: string;
+	picture: string;
+}
+
+const UserInfo: FC<UserInfoProps> = ({ name, picture }) => {
+	return (
+		<div className="flex align-center space-x-2 mx-5">
+			<img className="rounded-full" src={picture} alt={`Foto de perfil de ${name}`} />
+			<article className="text-white">
+				<h3>{name}</h3>
+				<p>Recursos Humanos</p>
+			</article>
+		</div>
+	);
+};
+
+/** Return the complete name of a user from a nested object */
+function getName(user: any) {
+	if (!user) return '';
+	const { first, last } = user.name;
+	return `${first} ${last}`;
+}
+
+export default Searchbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Sidebar from 'components/Sidebar/Sidebar';
+import Searchbar from 'components/Searchbar/Searchbar';
 
 const Home: NextPage = () => {
+	// / Use type any because we do not know the user date interface
+	const [ user, setUser ] = useState<any>();
+	const [ loading, setLoading ] = useState(true);
+	useEffect(() => {
+		// Fetch fake user data
+		(async function() {
+			try {
+				const result = await fetch('https://www.randomuser.me/api/?nat=es');
+				const json = await result.json();
+				setUser(json.results[0]);
+				console.log(json.results[0]);
+			} catch (error) {
+				console.error('Error getting the user');
+			} finally {
+				setLoading(false);
+			}
+		})();
+	}, []);
+
 	return (
 		<Fragment>
 			<Head>
@@ -12,6 +32,9 @@ const Home: NextPage = () => {
 			</Head>
 			<div className="flex">
 				<Sidebar />
+				<div className="w-full flex flex-col">
+					<Searchbar user={user} loading={loading} />
+				</div>
 			</div>
 		</Fragment>
 	);
