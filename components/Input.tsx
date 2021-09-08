03976c836846cd180c5f0ccf2dbad7a821aad11f
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-09-08 12:48:28 -0500

    Add custom styling props to Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -3,25 +3,35 @@ import React, { Fragment, FC, InputHTMLAttributes } from 'react';
 interface InputProps extends InputHTMLAttributes<{}> {
 	/** svg file source */
 	rightIcon?: string;
+	/** svg file source */
+	leftIcon?: string;
+
+	/** container className prop */
+	inputContainerClassName?: string;
 }
 
-const Input: FC<InputProps> = ({ rightIcon, className, ...rest }) => {
+const Input: FC<InputProps> = ({ leftIcon, rightIcon, className, inputContainerClassName, ...rest }) => {
 	return (
 		<Fragment>
-			<div className="flex items-center transition-colors ease duration-300  border-b-2 border-transparent focus-within:border-white">
-				<input
-					{...rest}
-					type="text"
-					className={`${className} bg-secondary text-white focus:outline-none w-full`}
-				/>
+			<div className={`${inputContainerClassName} flex items-center`}>
+				{leftIcon && (
+					<span className="mx-2">
+						<img aria-hidden src={leftIcon} alt="" />
+					</span>
+				)}
+				<input {...rest} type="text" className={`${className} bg-transparent focus:outline-none w-full`} />
 				{rightIcon && (
-					<span>
+					<span className="mx-2">
 						<img aria-hidden src={rightIcon} alt="" />
 					</span>
 				)}
 			</div>
 		</Fragment>
 	);
+};
+
+Input.defaultProps = {
+	className: ''
 };
 
 export default Input;
