c1a2931036fc82edd852cdd162fd4ff2df418b64
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-09-07 19:19:45 -0500

    Fix: Cannot use tailwind default colors because they were overrided by the brand ones

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ module.exports = {
   purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      "primary": "#323232",
-      "secondary": "#4FB9BB",
-      "terciary": "#E8EAF0"
+    extend: {
+      colors: {
+        "primary": "#323232",
+        "secondary": "#4FB9BB",
+        "terciary": "#E8EAF0"
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
